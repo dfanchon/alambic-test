23baa2cb34d1f642009dede144456c85b98b19f8
Didier Fanchon <didier.fanchon@webtales.fr>
2016-05-25 16:19:50 +0200

    wip

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+include './vendor/webtales/alambic/library/Alambic/Alambic.php';
+include './vendor/webtales/alambic/library/Alambic/Connector/Json.php';
+use Alambic\Alambic;
+use \Exception;
+
+$connectors = [
+  "mySimpleDB" => [
+    "connectorClass" => "Alambic\Connector\Json",
+    "basePath" => "./"
+  ]
+];
+
+$userModel = [
+  // Use a unique model name
+  "name" => "User",
+  // Whatever model description you want
+  "description" => "Writers",
+  // Fields list
+  "fields" => [
+    // The 'id' field
+    "id" => [
+      "type" => "String",
+      "required" => true,
+      "description" => "User Id"
+    ],
+    // The 'name' field
+    "name" => [
+      "type" => "String",
+      "description" => "User Name"
+    ],
+    // We add a relational field stored in another type
+    "posts" => [
+      "type" => "Post",
+      // The relation can be hasOne or hasMany like here
+      "multivalued" => true,
+      // We can only query the text of the post
+      "args" => [
+        "text" => [
+          "type"=> "String"
+        ]
+      ],
+      // The foreign key is the "author" field in the Post type
+      "relation" => [
+        "author" => "id"
+      ]
+    ]
+  ],
+  // Can we query this model directly?
+  "expose" => true,
+  // The single endpoint allows to request for one user at a time
+  "singleEndpoint" => [
+    "name" => "user",
+    // This endpoint only takes a user Id as an argument
+    "args" => [
+      "id" => [
+        "type" => "String",
+        "required" => true,
+        "description" => "User Id"
+      ]
+    ]
+  ],
+  // The connector used to fetch/write the data
+  "connector" => [
+    "type" => "mySimpleDB",
+    "configs" => [
+      "fileName" => "users.json"
+    ]
+  ]
+];
+
+$postModel = [
+  "name" => "Post",
+  "description" => "Blog Posts",
+  "fields" => [
+    "id" => [
+      "type" => "String",
+      "required" => true,
+      "description" => "Post Id"
+    ],
+    "text" => [
+      "type" => "String",
+      "description" => "Post Text"
+    ],
+    "author" => [
+      "type" => "String",
+      "description" => "Author Id"
+    ]
+  ],
+  "expose" => true,
+  "singleEndpoint" => [
+    "name" => "post",
+    "args" => [
+      "id" => [
+        "type" => "String",
+        "required" => true,
+        "description" => "Post Id"
+      ]
+    ]
+  ],
+  "multiEndpoint" => [
+    "name" => "posts",
+    "args" => [
+    ]
+  ],
+  "connector" => [
+    "type" => "mySimpleDB",
+      "configs" => [
+        "fileName" => "posts.json"
+      ]
+    ]
+  ];
+
+  $alambicConfig=[
+    "alambicConnectors" => $connectors,
+    "alambicTypeDefs" => [
+      "User" => $userModel,
+      "Post" => $postModel
+    ]
+  ];
+
+  $data = $_GET;
+
+  $requestString = isset($data['query']) ? $data['query'] : null;
+  $operationName = isset($data['operation']) ? $data['operation'] : null;
+  $variableValues = isset($data['variables']) ? $data['variables'] : null;
+
+  $alambic = new Alambic($alambicConfig);
+  $result = $alambic->execute($requestString,$variableValues,$operationName);
+
+  header('Content-Type: application/json');
+  echo json_encode($result);
